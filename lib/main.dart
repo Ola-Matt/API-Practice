3cc2387be0219eadd33dd1a1e335aea836646937
Olaoluwa <tokiolaoluwa01@gmail.com>
2022-04-19 12:45:59 +0100

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Layout',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Layout Demo'),
+        ),
+        body: const Center(
+          child: Text('Demo'),
+        ),
+      ),
+    );
+  }
+}
